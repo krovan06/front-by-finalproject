833f57ff7c03bba5f66f3bff4086da53f9923caa
sohhhhhhhhhh <mildex@tutamail.com>
2025-01-27 21:02:29 +1000

    ГЛАЗИК

--- js/reg.js
+++ js/reg.js
@@ -9,3 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
     showMaskOnHover: false // Не показывать маску при наведении
   }).mask(phoneInput);
 });
+
+
+// Модальное окно
+const modal = document.getElementById('agreement-modal');
+const modalClose = document.querySelector('.close');
+const agreementLink = document.getElementById('agreement-link');
+const checkbox = document.getElementById('agreement-checkbox');
+const confirmButton = document.getElementById('confirm-btn');
+
+// Открытие модального окна
+agreementLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = 'block';
+});
+
+// Закрытие модального окна
+modalClose.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// Проверка чекбокса и активация кнопки
+checkbox.addEventListener('change', () => {
+  confirmButton.disabled = !checkbox.checked;
+});
